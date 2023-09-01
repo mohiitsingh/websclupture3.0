8af265b3560e4b1f34c1daff06fc6f62d7ee1d98
mohiitsingh <ms6065097@gmail.com>
2023-09-02 01:06:33 +0530

    animation added

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-
 // scroll feature
-window.addEventListener('scroll',() =>{
-    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 100);
-})
-
-
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 100);
+});
 
 // toggle button feature
 const toggleButton = document.querySelector("#nav-toggle");
-const navLinks = document.getElementById('nav-links');
+const navLinks = document.getElementById("nav-links");
 
-toggleButton.addEventListener('click',() => {
-    navLinks.classList.toggle('active');
-})
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
 //  pre and next button feature
-const productContainers = [...document.querySelectorAll('.product-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
+const productContainers = [...document.querySelectorAll(".product-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
 
 productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
 
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
 
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
-
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
 
 //whatsapp
 
-document.getElementById("get-quote-button2").addEventListener("click", function() {
+document
+  .getElementById("get-quote-button2")
+  .addEventListener("click", function () {
     var phoneNumber = "9424457498"; // Replace with the actual phone number
-    var instantQuoteMessage = "Thank you for requesting an instant quote! Please tell us How we can help ?";
+    var instantQuoteMessage =
+      "Thank you for requesting an instant quote! Please tell us How we can help ?";
 
     var encodedMessage = encodeURIComponent(instantQuoteMessage);
-    var whatsappUrl = "https://wa.me/" + phoneNumber + "?text=" + encodedMessage;
+    var whatsappUrl =
+      "https://wa.me/" + phoneNumber + "?text=" + encodedMessage;
 
     window.open(whatsappUrl, "_blank");
-});
+  });
